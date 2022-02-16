1df686d485617abd1facb62beb195ca5b06d1342
Hozan-Batan <cav.cavov@mail.ru>
2022-02-16 22:34:53 +0400

    First

---
+++ Task8-a/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            int[] arr1 = { 27, 51, 33, 8, 11, 15, 10, 56, 23, 78, 23, 54, 32, 234, 434, 23, 124};
+            Sort(arr1);
+            foreach(int item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static int[] Sort(int[] a)
+        {
+            int m = 0;
+            int i = 1;
+            for (int b = 1; b < a.Length; b++)
+            {
+                foreach (int item in a)
+                {
+                    if (item > a[i])
+                    {
+                        int temp = a[m];
+                        a[m] = a[i];
+                        a[i] = temp;
+
+                    }
+                    if (i == (a.Length-1))
+                    {
+                        break;
+                    }
+                    i++;
+                    m++;
+                }
+                i = 1;
+                m = 0;
+                
+            }
+            return a;
+        }
+    }
+}
---
+++ Task8-b/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task8_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sozleri daxil edin!");
+            string[] tema = { "Alma", "Vanna", "Heyva", "Lampa" };
+            char herf = 'v';
+            Console.WriteLine(CharinIndex(tema, herf));
+        }
+        static string CharinIndex(string[] a, char b)
+        {
+            string words = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                foreach (char item in a[i])
+                {
+                    char p = item;
+                    if (p == b)
+                    {
+                        words += a[i] + " ";
+                    }
+                }
+            }return words;
+        }
+    }
+}
